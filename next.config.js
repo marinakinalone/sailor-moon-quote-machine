60046bdcda2fe578b9b2629a17ede6682913c622
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2022-06-04 14:47:24 +0200

    change config for image loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  // https://github.com/vercel/next.js/issues/21079
+  // Remove this workaround whenever the issue is fixed
+  images: {
+    loader: 'imgix',
+    path: '/',
+  },
 }
-
-module.exports = nextConfig
